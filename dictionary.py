cfc5f4bcfbe937a0f2e2bf7ca19bed81982bf33b
algorythmist <dimitri@tecacet.com>
2022-02-20 18:29:38 -0800

    removed unused code

--- dictionary.py
+++ dictionary.py
@@ -1,6 +1,3 @@
-from itertools import chain
-import random
-
 def read_dictionary(filename='wordle_dict.txt', word_length=5):
     with open(filename, 'r') as file:
         words = file.readlines()
@@ -21,40 +18,6 @@ def filter_word(word: str, score: dict) -> bool:
 
 def filter_dictionary(dictionary, score) -> bool:
     return {w for w in dictionary if filter_word(w, score)}
-
-
-class Dictionary:
-
-    def filter(self, score):
-        pass
-
-    def sample(self):
-        pass
-
-
-class SetDictionary(Dictionary):
-
-    def __init__(self, words):
-        self.words = set(words)
-
-    def __len__(self):
-        return len(self.words)
-
-    def filter(self, score):
-        return SetDictionary(filter_dictionary(self.words, score))
-
-    def sample(self):
-        # TODO: Sampling from a set is deprecated. Find new way
-        return random.sample(self.words, 1)[0]
-
-
-def matches_exact_letters(word, score):
-    for i, (letter, value) in enumerate(score):
-        if value == 1 and word[i] != letter:
-            return False
-        if value == 0 and word[i] == letter:
-            return False
-    return True
 
 
 
--- test_dictionary.py
+++ test_dictionary.py
@@ -13,13 +13,3 @@ class DictionaryTestCase(unittest.TestCase):
         five_letter_words = read_dictionary()
         new_dict = filter_dictionary(five_letter_words, [('W', -1), ('O', -1), ('R', -1), ('D', -1), ('Y', -1)])
         self.assertEqual(644, len(new_dict))
-
-
-    def test_set(self):
-        words = ['ABATE', 'DISCO', 'FALSE', 'NEEDY', 'SKATE', 'WORDY']
-        dictionary = SetDictionary(words)
-        filtered_dictionary = dictionary.filter([('A', 0), ('N', -1), ('I', -1), ('M', -1), ('E', 1)])
-        self.assertEqual(2, len(filtered_dictionary))
-        self.assertEqual({'SKATE', 'FALSE'}, filtered_dictionary.words)
-
-
